ceee0dcdaa9308ce3f14a39d65b07379d482d83c
shivs2000 <singhshivam0666@gmail.com>
2022-09-08 12:14:21 +0530

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+function App() {
+  return (
+    <Container>
+      <Routes>
+        <Route />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
