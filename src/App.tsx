578356f49c8e4827769bb93b8c6dbe7dadbf1a1c
shivs2000 <singhshivam0666@gmail.com>
2022-09-08 14:13:53 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
 function App() {
   return (
     <Container>
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/store" element={<Store />} />
       </Routes>
     </Container>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1> welcome to</h1>Home
+    </div>
+  );
+};
+
+export default Home;
